a6323d84e7a22d0d7a7c1f036f5a84541c1495f3
najibean <najibullah89@gmail.com>
2021-09-02 03:36:33 +0700

    update how to generate id from last index

--- models/productModel.js
+++ models/productModel.js
@@ -2,49 +2,48 @@ const data = require('./../data/mock.json')
 const { writeDataToFile } = require('./../utils')
 
 const findAll = () => {
-  return new Promise((resolve, reject) => {
-    resolve(data)
-  })
+	return new Promise((resolve, reject) => {
+		resolve(data)
+	})
 }
 
-const findById = (id) => {
-  return new Promise((resolve, reject) => {
-    const product = data.find((el) => el.id === id)
-    resolve(product)
-  })
+const findById = id => {
+	return new Promise((resolve, reject) => {
+		const product = data.find(el => el.id === id)
+		resolve(product)
+	})
 }
 
-// @BUG -- id yang baru dicreate harus tidak boleh berdasarkan panjang length
-const create = (product) => {
-  return new Promise((resolve, reject) => {
-    const newProduct = { id: data.length + 1, ...product }
-    data.push(newProduct)
-    writeDataToFile('./data/mock.json', data)
-    resolve(newProduct)
-  })
+const create = product => {
+	return new Promise((resolve, reject) => {
+		const newProduct = { id: data[data.length - 1].id + 1, ...product }
+		data.push(newProduct)
+		writeDataToFile('./data/mock.json', data)
+		resolve(newProduct)
+	})
 }
 
 const update = (id, product) => {
-  return new Promise((resolve, reject) => {
-    const index = data.findIndex((el) => el.id === id)
-    data[index] = { id, ...product }
-    writeDataToFile('./data/mock.json', data)
-    resolve(data[index])
-  })
+	return new Promise((resolve, reject) => {
+		const index = data.findIndex(el => el.id === id)
+		data[index] = { id, ...product }
+		writeDataToFile('./data/mock.json', data)
+		resolve(data[index])
+	})
 }
 
-const remove = (id) => {
-  return new Promise((resolve, reject) => {
-    const product = data.filter((el) => el.id !== id)
-    writeDataToFile('./data/mock.json', product)
-    resolve()
-  })
+const remove = id => {
+	return new Promise((resolve, reject) => {
+		const product = data.filter(el => el.id !== id)
+		writeDataToFile('./data/mock.json', product)
+		resolve()
+	})
 }
 
 module.exports = {
-  findAll,
-  findById,
-  create,
-  update,
-  remove,
+	findAll,
+	findById,
+	create,
+	update,
+	remove
 }
--- utils.js
+++ utils.js
@@ -1,31 +1,32 @@
 const fs = require('fs')
+const data = require('./data/mock.json')
 
 const writeDataToFile = (filename, content) => {
-  fs.writeFileSync(filename, JSON.stringify(content), 'utf8', (err) => {
-    if (err) {
-      console.log(err)
-    }
-  })
+	fs.writeFileSync(filename, JSON.stringify(content), 'utf8', err => {
+		if (err) {
+			console.log(err)
+		}
+	})
 }
 
-const getPostData = (req) => {
-  return new Promise((resolve, reject) => {
-    try {
-      let body = ''
-      req.on('data', (chunk) => {
-        body += chunk.toString()
-      })
-      req.on('end', () => {
-        const productInput = JSON.parse(body)
-        resolve(productInput)
-      })
-    } catch (error) {
-      console.log(error)
-    }
-  })
+const getPostData = req => {
+	return new Promise((resolve, reject) => {
+		try {
+			let body = ''
+			req.on('data', chunk => {
+				body += chunk.toString()
+			})
+			req.on('end', () => {
+				const productInput = JSON.parse(body)
+				resolve(productInput)
+			})
+		} catch (error) {
+			console.log(error)
+		}
+	})
 }
 
 module.exports = {
-  writeDataToFile,
-  getPostData
+	writeDataToFile,
+	getPostData
 }
